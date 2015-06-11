5662484e2cfec9ff52796af429b4ea9e42e6b97c
Thomas Rabaix <thomas.rabaix@gmail.com>
2015-06-12 00:31:05 +0200

    add Application Lifecycle

--- goapp.go
+++ goapp.go
@@ -1,44 +1,182 @@
+// The package try to normalize how an application should start without
+// providing any convention about how each steps should be used.
+//
+// The process will go through 5 steps:
+//  - Init: Initialize application: register flags, no logic should be done here
+//  - Register : Register components that does not required configuration settings
+//  - Config   : Read configuration and defined main services from this configuration
+//  - Run      : Run the main program loop
+//  - Exit     : (not yet implemented
 package goapp
 
-type App struct {
-    // contains the original closure to generate the service
-    values map[string]AppFunc
+import (
+	"errors"
+	"fmt"
+	"runtime/debug"
+	"sync"
+)
 
-    // contains the instanciated services
-    services map[string]interface{}
+const (
+	Init     = 10 // Initialize application: register flags, no logic should be done here
+	Register = 20 // Register components that does not required configuration settings
+	Config   = 30 // Read configuration and defined main services from this configuration
+	Run      = 40 // Run the main program loop
+	Exit     = 50 // Exit the program
+)
+
+// The structure contains all services build by the AppFunc function, the service is initialized when get Get
+// method is called.
+type App struct {
+	state    int
+	values   map[string]AppFunc     // contains the original closure to generate the service
+	services map[string]interface{} // contains the instantiated services
 }
 
 type AppFunc func(app *App) interface{}
 
 func (app *App) Set(name string, f AppFunc) {
-    if _, ok := app.services[name]; ok {
-        panic("Cannot overwrite initialized service")
-    }
+	if _, ok := app.services[name]; ok {
+		panic("Cannot overwrite initialized service")
+	}
 
-    app.values[name] = f
+	app.values[name] = f
 }
 
 func (app *App) Get(name string) interface{} {
-    if _, ok := app.values[name]; !ok {
-        panic("The service does not exist")
-    }
+	if _, ok := app.values[name]; !ok {
+		panic(fmt.Sprintf("The service does not exist: %s", name))
+	}
 
-    if _, ok := app.services[name]; !ok {
-        app.services[name] = app.values[name](app)
-    }
+	if _, ok := app.services[name]; !ok {
+		app.services[name] = app.values[name](app)
+	}
 
-    return app.services[name]
+	return app.services[name]
 }
 
 func (app *App) GetString(name string) interface{} {
-    return app.Get(name).(string)
+	return app.Get(name).(string)
 }
 
 func NewApp() *App {
-    app := App{
-        services: make(map[string]interface{}),
-        values: make(map[string]AppFunc),
-    }
+	app := App{
+		services: make(map[string]interface{}),
+		values:   make(map[string]AppFunc),
+	}
 
-    return &app
+	return &app
+}
+
+type LifecycleFun func(app *App) error
+
+type Lifecycle struct {
+	init     []LifecycleFun
+	register []LifecycleFun
+	config   []LifecycleFun
+	run      []LifecycleFun
+	exit     []LifecycleFun
+}
+
+// register a new LifecycleFun function to a step
+func (l *Lifecycle) Add(t int, f LifecycleFun) {
+	switch t {
+	case Init:
+		l.init = append(l.init, f)
+	case Register:
+		l.register = append(l.register, f)
+	case Config:
+		l.config = append(l.config, f)
+	case Run:
+		l.run = append(l.run, f)
+	case Exit:
+		l.exit = append(l.exit, f)
+	}
+}
+
+// Start the different step, the each LifecycleFun defined in the run step will be started in a dedicated goroutine.
+// The Go function will exit the program
+func (l *Lifecycle) Go(app *App) int {
+
+	fmt.Printf(">>> GoApp vX.X\n")
+	fmt.Printf(">>> GoApp - Init\n")
+	app.state = Init
+	for _, f := range l.init {
+		f(app)
+	}
+
+	fmt.Printf(">>> GoApp - Register\n")
+	app.state = Register
+	for _, f := range l.register {
+		f(app)
+	}
+
+	fmt.Printf(">>> GoApp - Configuration\n")
+	app.state = Config
+	for _, f := range l.config {
+		f(app)
+	}
+
+	fmt.Printf(">>> GoApp - Run\n")
+	app.state = Run
+	var wg sync.WaitGroup
+
+	results := make([]chan error, len(l.run)) // create a pool of channel to handle each return
+
+	for p, f := range l.run {
+		results[p] = make(chan error, 1)
+
+		wg.Add(1)
+		go func(f LifecycleFun, c chan error) {
+
+			defer func() {
+				if r := recover(); r != nil {
+					message := fmt.Sprintf("Panic recovered, message=%s\n", r)
+
+					c <- errors.New(message + string(debug.Stack()[:]))
+				}
+
+				wg.Done()
+			}()
+
+			c <- f(app)
+
+		}(f, results[p])
+	}
+
+	wg.Wait()
+
+	hasError := false
+	for _, c := range results {
+		err := <-c
+
+		if err != nil {
+			fmt.Printf(">>> GoApp - Error: %s\n", err)
+			hasError = true
+		}
+	}
+
+	fmt.Printf(">>> GoApp - Exit\n")
+	app.state = Exit
+	for _, f := range l.exit {
+		f(app)
+	}
+
+	fmt.Printf(">>> GoApp - done!\n")
+
+	// check for errors
+	if hasError {
+		return 1
+	}
+
+	return 0
+}
+
+func NewLifecycle() *Lifecycle {
+	return &Lifecycle{
+		init:     make([]LifecycleFun, 0),
+		register: make([]LifecycleFun, 0),
+		config:   make([]LifecycleFun, 0),
+		run:      make([]LifecycleFun, 0),
+		exit:     make([]LifecycleFun, 0),
+	}
 }
--- goapp_test.go
+++ goapp_test.go
@@ -1,71 +1,144 @@
 package goapp
 
 import (
-    "testing"
-    "github.com/stretchr/testify/assert"
-    "os"
-    "log"
+	"container/list"
+	"errors"
+	"github.com/stretchr/testify/assert"
+	"log"
+	"os"
+	"testing"
+	"time"
 )
 
 type User struct {
-    name string
+	name string
 }
 
 func Test_App(t *testing.T) {
-    
-    app := NewApp()
 
-    app.Set("logger", func(app *App) interface{} {
-        return log.New(os.Stdout, "prefix", log.LstdFlags)
-    })
+	app := NewApp()
 
-    app.Set("map", func(app *App) interface{} {
-        m := make(map[string]string)
-        m["test"] = "test"
+	app.Set("logger", func(app *App) interface{} {
+		return log.New(os.Stdout, "prefix", log.LstdFlags)
+	})
 
-        return m
-    })
+	app.Set("map", func(app *App) interface{} {
+		m := make(map[string]string)
+		m["test"] = "test"
 
-    app.Set("key", func(app *App) interface{} {
-        return "Salut"
-    })
+		return m
+	})
 
-    assert.IsType(t, app.Get("logger").(*log.Logger), new(log.Logger))
+	app.Set("key", func(app *App) interface{} {
+		return "Salut"
+	})
 
-    m := app.Get("map").(map[string]string)
-    m["foo"] = "bar"
-    m["test"] = "hello"
+	assert.IsType(t, app.Get("logger").(*log.Logger), new(log.Logger))
 
-    a := app.Get("map").(map[string]string)
-    assert.Equal(t, a["test"], "hello") 
+	m := app.Get("map").(map[string]string)
+	m["foo"] = "bar"
+	m["test"] = "hello"
 
-    assert.Equal(t, "Salut", app.GetString("key"))
+	a := app.Get("map").(map[string]string)
+	assert.Equal(t, a["test"], "hello")
+
+	assert.Equal(t, "Salut", app.GetString("key"))
 }
 
 func Test_NoReference(t *testing.T) {
-    app := NewApp()
+	app := NewApp()
 
-    app.Set("user", func(app *App) interface{} {
-        return User{
-            name: "Thomas",
-        }
-    })
+	app.Set("user", func(app *App) interface{} {
+		return User{
+			name: "Thomas",
+		}
+	})
 
-    app.Set("user.reference", func(app *App) interface{} {
-        return &User{
-            name: "Thomas",
-        }
-    })
+	app.Set("user.reference", func(app *App) interface{} {
+		return &User{
+			name: "Thomas",
+		}
+	})
 
-    u := app.Get("user").(User)
-    u.name = "Fred"
+	u := app.Get("user").(User)
+	u.name = "Fred"
 
-    u = app.Get("user").(User)
-    assert.NotEqual(t, "Fred", u.name)
+	u = app.Get("user").(User)
+	assert.NotEqual(t, "Fred", u.name)
 
-    u_r := app.Get("user.reference").(*User)
-    u_r.name = "Fred"
+	u_r := app.Get("user.reference").(*User)
+	u_r.name = "Fred"
 
-    u_r = app.Get("user.reference").(*User)
-    assert.Equal(t, "Fred", u_r.name)
+	u_r = app.Get("user.reference").(*User)
+	assert.Equal(t, "Fred", u_r.name)
+}
+
+func Test_LifeCycle(t *testing.T) {
+	l := NewLifecycle()
+
+	l.Add(Init, func(app *App) error {
+		app.Set("hello", func(app *App) interface{} {
+			return "world"
+		})
+
+		return nil
+	})
+
+	l.Add(Register, func(app *App) error {
+		// The register need to value from the initial loop
+		hello := app.GetString("hello") // so the hello service will be initialized
+
+		assert.Equal(t, hello, "world")
+
+		app.Set("testing", func(app *App) interface{} {
+			return t
+		})
+
+		return nil
+	})
+
+	l.Add(Config, func(app *App) error {
+		tester := app.Get("testing").(*testing.T) // always need to cast
+
+		assert.Equal(t, tester, t)
+
+		app.Set("messages", func(app *App) interface{} {
+			return list.New()
+		})
+
+		return nil
+	})
+
+	l.Add(Run, func(app *App) error {
+		messages := app.Get("messages").(*list.List)
+
+		messages.PushBack("Run1: sleep for 0.5s")
+		time.Sleep(500 * time.Millisecond)
+		messages.PushBack("Run1: Wake up ...")
+
+		return nil
+	})
+
+	l.Add(Run, func(app *App) error {
+		messages := app.Get("messages").(*list.List)
+
+		messages.PushBack("Run2: sleep for 0.5s")
+		time.Sleep(500 * time.Millisecond)
+		messages.PushBack("Run2: Wake up ...")
+
+		return errors.New("this is a error")
+	})
+
+	l.Add(Run, func(app *App) error {
+		panic("take this !!")
+	})
+
+	app := NewApp()
+
+	r := l.Go(app)
+
+	messages := app.Get("messages").(*list.List)
+	assert.Equal(t, 4, messages.Len())
+
+	assert.Equal(t, r, 1) // an error is present
 }
